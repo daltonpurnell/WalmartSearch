f1d9baa45f22299c6919bca635a60297ffe79dcd
daltonpurnell <daltonpurnell@live.com>
2018-05-13 08:21:54 -0600

    adds fonts & colors to project

--- WalmartSearch/Helpers/Constants.swift
+++ WalmartSearch/Helpers/Constants.swift
@@ -23,4 +23,17 @@ struct Constants {
         static let searchPath:String = "/v1/search"
         static let recommendationsPath:String = "/v1/nbp"
     }
+    
+    struct Fonts {
+        static let h1:UIFont? = UIFont(name: "Bogle-Black", size: 22)
+        static let h2:UIFont? = UIFont(name: "Bogle-Bold", size: 18)
+        static let p1:UIFont? = UIFont(name: "Bogle-Regular", size: 14)
+        static let p2:UIFont? = UIFont(name: "Bogle-Regular", size: 12)
+    }
+    
+    struct Colors {
+        static let walmartBlue:UIColor = UIColor(red: 0.0/255.0, green: 113.0/255.0, blue: 206.0/255.0, alpha: 1)
+        static let sparkYellow:UIColor = UIColor(red: 255.0/255.0, green: 194.0/255.0, blue: 32.0/255.0, alpha: 1)
+        static let darkBlue:UIColor = UIColor(red: 4.0/255.0, green: 30.0/255.0, blue: 66.0/255.0, alpha: 1)
+    }
 }
--- WalmartSearch/Helpers/StringExtension.swift
+++ WalmartSearch/Helpers/StringExtension.swift
@@ -24,3 +24,4 @@ extension String {
         return htmlToAttributedString?.string ?? ""
     }
 }
+
